314f49f1c92caf1abb260849e3d7c4aaab80b999
borisdaev <92102248+borisdaev@users.noreply.github.com>
2021-12-22 12:48:26 +0100

    proektna_zavrshen_del

--- gallery.js
+++ gallery.js
@@ -1,1 +1,10 @@
-
+/*
+$(document).ready(function() {
+    $("#poraka").mouseover(function() {
+    $("#poraka").css("color", "darkred");
+});
+    $("#poraka").mouseout(function() {
+    $("#poraka").css("color", "white");
+});
+});
+*/
